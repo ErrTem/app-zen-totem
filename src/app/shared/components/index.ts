38bebdf9cb5e58b28b33534e579a5c2418248fab
ErrTem <nitroxess@gmail.com>
2023-07-16 22:59:15 +0300

    snackbar not working need to fix

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,3 +1,4 @@
 export * from './login-form/login-form.component';
 export * from './product-card/product-card.component';
 export * from './slider/slider.component';
+export * from './snack-bar/snack-bar.component';
--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -2,8 +2,10 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductInterface } from '@core/interfaces/product.interface';
 import { MatButtonModule } from '@angular/material/button';
+import { SnackBarComponent } from '../snack-bar/snack-bar.component';
 
 @Component({
   selector: 'app-product-card',
@@ -13,11 +15,18 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./product-card.component.sass'],
 })
 export class ProductCardComponent {
+  private readonly durationInSeconds = 1000;
   @Input() products!: ProductInterface[];
 
-  constructor() {}
+  constructor(private readonly snackBar: MatSnackBar) {}
   //todo add styles to mat-button to change color
   addToBasket(product: ProductInterface) {
     // Implement your logic for adding the product to the basket
+  }
+
+  openSnackBar() {
+    this.snackBar.openFromComponent(SnackBarComponent, {
+      duration: this.durationInSeconds * 200,
+    });
   }
 }
