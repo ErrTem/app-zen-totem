b96c2a9b055b3b35d7abf769370640f36a76516e
ErrTem <nitroxess@gmail.com>
2023-08-11 22:32:00 +0300

    replace logic to backend.service

--- src/app/core/services/backend.service.ts
+++ src/app/core/services/backend.service.ts
@@ -3,26 +3,19 @@ import { BackendPersonInterface } from '@core/interfaces';
 import { HttpClient } from '@angular/common/http';
 import { delay, Observable, of } from 'rxjs';
 
-export const BACKEND_SERVICE_TOKEN = new InjectionToken<BackendService>('backend_service_token');
+export const BACKEND_SERVICE_TOKEN = new InjectionToken<BackendService>(
+  'backend_service_token'
+);
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 //todo temporary until backend will be implemented;
 export class BackendService {
-  private readonly mockData: BackendPersonInterface[] = [];
+  constructor(private readonly http: HttpClient) {}
 
-  constructor(
-    private readonly http: HttpClient,
-  ) {
+  getPeople(): Observable<BackendPersonInterface[]> {
+    return this.http.get<BackendPersonInterface[]>('assets/data/people.json');
   }
-
-  getPeopleBatch(startIndex: number, batchSize: number): Observable<BackendPersonInterface[]> {
-    // Simulate an asynchronous delay and return a batch of people
-    return of(this.mockData.slice(startIndex, startIndex + batchSize)).pipe(
-      delay(500) // Simulate network delay
-    );
-  }
-
 }
--- src/app/features/event-speakers/event-speakers/event-speakers.component.ts
+++ src/app/features/event-speakers/event-speakers/event-speakers.component.ts
@@ -1,40 +1,39 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { BackendPersonInterface } from '@core/interfaces';
+import { BackendService } from '@core/services/backend.service';
+import { CHUNK_SIZE } from '@shared/constants';
 
 @Component({
   selector: 'app-event-speakers',
   templateUrl: './event-speakers.component.html',
-  styleUrls: ['./event-speakers.component.sass']
+  styleUrls: ['./event-speakers.component.sass'],
 })
 export class EventSpeakersComponent implements OnInit {
   allPeople: BackendPersonInterface[] = [];
   displayedPeople: BackendPersonInterface[] = [];
-  chunkSize = 8;
   currentChunkIndex = 0;
 
-  constructor(
-    private readonly http: HttpClient,
-  ) {
-  }
+  constructor(private readonly backendService: BackendService) {}
 
   ngOnInit(): void {
     this.fetchData();
   }
-//todo refactor to backend.service; improve html; add correct sass; add loading by 8 pesons and loader; simulate api call by delay
+
   fetchData() {
-    this.http.get<BackendPersonInterface[]>('assets/data/people.json').subscribe(data => {
+    this.backendService.getPeople().subscribe((data) => {
       this.allPeople = data;
       this.loadNextChunk();
     });
   }
 
   loadNextChunk() {
-    const startIndex = this.currentChunkIndex * this.chunkSize;
-    const endIndex = startIndex + this.chunkSize;
+    const startIndex = this.currentChunkIndex * CHUNK_SIZE;
+    const endIndex = startIndex + CHUNK_SIZE;
 
     if (startIndex < this.allPeople.length) {
-      this.displayedPeople = this.displayedPeople.concat(this.allPeople.slice(startIndex, endIndex));
+      this.displayedPeople = this.displayedPeople.concat(
+        this.allPeople.slice(startIndex, endIndex)
+      );
       this.currentChunkIndex++;
     }
   }
